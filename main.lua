615ca6e31c87bf464653bd3e2c73fe581223eec0
mans <103044423+mantrippin@users.noreply.github.com>
2022-04-06 17:53:36 +0000

    .

--- main.lua
+++ main.lua
@@ -3,22 +3,28 @@ print("Num1:")
 local num1 = io.read()
 print("num2:")
 local num2 = io.read()
-print("Choose operator: + / - / * / /")
+--Above this is just the numbers and selecting them
+print("Choose symbol: + - * or x, / or divide")
 local operator = io.read()
 if operator == "+" then
    local add = num1 + num2
    print(add)     
 end 
+--This is for adding numbers
 if operator == "-" then
    local subtract = num1 - num2
    print(subtract)   
 end
-if operator == "*" then
+--Subtracting numbers
+if operator == "*" or "x" then
    local multiply = num1 * num2
    print(multiply)
 end   
-if operator == "/" then  
+--Multiplying 
+if operator == "/" or "divide" then  
    local devide = num1 / num2
    print(devide) 
+--And finally dividing
+print ("Made by mantrippin")
 end
 io.read()    
